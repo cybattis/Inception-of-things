ade8fb8e357ec27061b996288e434d09733a0e52
cybattis <cybattis@cybattis.lan>
2025-02-16 17:16:36 +0100

    spilit script

--- init-vm.sh
+++ init-vm.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -euo pipefail
+
+echo -e "\033[1;3;34m=== Init p1 & p2 script starting... ===\033[0m"
 
 sudo apt update && apt upgrade
 sudo apt install -y git vim apt-transport-https ca-certificates curl gnupg
 
 # Install Vagrant
+echo -e "\033[1;3;34m=== Install Vagrant ===\033[0m"
+
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
@@ -11,6 +16,8 @@ sudo apt update
 sudo apt-get install -y vagrant
 
 # Install provider VirtualBox
+echo -e "\033[1;3;34m=== Install VirtualBox ===\033[0m"
+
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
 
@@ -18,28 +25,8 @@ sudo apt update
 sudo apt-get install -y virtualbox-7.0
 sudo usermod -aG vboxusers $USER
 
-# Install docker
-# Add Docker's official GPG key:
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo -e "\033[1;3;34m=== Cleaning... ===\033[0m"
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Install kublect
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
-sudo apt-get update
-sudo apt-get install -y kubectl
-
-# Install K3d
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-# Cleaning
 sudo apt -y clean && sudo apt -y autoremove && sudo apt -y autoclean
+
+echo -e "\033[1;3;34m=== Done! ===\033[0m"
---
+++ p3/scripts/init-p3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -euo pipefail
+
+echo -e "\033[1;3;34m=== Init p3 script starting ===\033[0m"
+
+sudo apt-get install net-tools -y
+
+echo -e "\033[1;3;34m=== Install docker ===\033[0m"
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo -e "\033[1;3;34m=== Install kublect ===\033[0m"
+# Download latest release
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo -e "\033[1;3;34m=== Install K3d ===\033[0m"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+echo -e "\033[1;3;34m=== Cleaning... ===\033[0m"
+sudo apt -y clean && sudo apt -y autoremove && sudo apt -y autoclean
+
+echo -e "\033[1;3;34m=== Done ===\033[0m"
+
+echo -e "\033[1;3;34m=== Init k3d cluster... ===\033[0m"
+k3d cluster create mycluster --agents 2
+
+kubectl create namespace argocd
+kubectl create namespace dev
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo -e "\033[1;3;34m=== Waiting Argo CD Setup ===\033[0m"
+
+# params="-n argocd -l app.kubernetes.io/name=argocd-server --timeout=10m"
+# kubectl wait --for=condition=available deployment $params
+# kubectl wait --for=condition=ready pod $params
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
