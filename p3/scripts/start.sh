d9f6751855988bb1d1c7e4487904a0877926cd50
cybattis <cybattis@student.42lyon.fr>
2025-02-16 22:11:47 +0100

    fix script

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -15,7 +15,8 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl wait pods --all -n argocd --for condition=Ready --timeout=300s
 kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
 
-INIT_PASSWORD=$(argocd admin initial-password -n argocd)
+INIT_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d;)
+echo $INIT_PASSWORD
 
 argocd login localhost:8080 --username admin --password $INIT_PASSWORD
 
