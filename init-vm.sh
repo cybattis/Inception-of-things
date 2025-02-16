b1ffa272283cee9a640dfdb8998b2a7462e21a3c
Cyril <cyril@cybattis.lan>
2025-02-16 12:42:06 +0100

    update init script

--- init-vm.sh
+++ init-vm.sh
@@ -8,5 +8,15 @@ sudo apt install -y git vim
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
+sudo apt update
+sudo apt-get install -y vagrant
+
 # Install provider VirtualBox
-apt-get install -y virtualbox
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
+
+sudo apt update
+sudo apt-get install -y virtualbox-7.1
+sudo usermod -aG vboxusers $USER
+
+sudo apt -y clean && sudo apt -y autoremove && sudo apt -y autoclean
