df82ffacb7d15f4efd6eec8102ec8f76ce58b2e0
Nathan Jennes <njennes@student.42lyon.fr>
2025-02-16 19:57:13 +0100

    Fix Url for argocd manifest

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -11,6 +11,6 @@ echo -e "\033[1;3;34m=== Init Argocd ===\033[0m"
 
 # Argocd
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubcontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait pods --all -n argocd --for condition=Ready --timeout=300s
 kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
