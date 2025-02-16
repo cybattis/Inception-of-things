2d86cfae6af15ab5d96ebbfa0a344fa59a792dc4
cybattis <cybattis@student.42lyon.fr>
2025-02-16 15:53:40 +0100

    update init script p3

--- init-vm.sh
+++ init-vm.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
 sudo apt update && apt upgrade
+sudo apt install -y git vim apt-transport-https ca-certificates curl gnupg
 
-sudo apt install -y git vim
-
-# See instructions at https://developer.hashicorp.com/vagrant/downloads
+# Install Vagrant
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
@@ -19,4 +18,28 @@ sudo apt update
 sudo apt-get install -y virtualbox-7.0
 sudo usermod -aG vboxusers $USER
 
+# Install docker
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install kublect
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install K3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Cleaning
 sudo apt -y clean && sudo apt -y autoremove && sudo apt -y autoclean
