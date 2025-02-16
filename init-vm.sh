7dc462e7c3b5e1cf6c545821d24d5d9b8d9eb099
cybattis <cybattis@student.42lyon.fr>
2025-02-16 12:59:28 +0100

    update init script vb

--- init-vm.sh
+++ init-vm.sh
@@ -16,7 +16,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http:/
 wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
 
 sudo apt update
-sudo apt-get install -y virtualbox-7.1
+sudo apt-get install -y virtualbox-7.0
 sudo usermod -aG vboxusers $USER
 
 sudo apt -y clean && sudo apt -y autoremove && sudo apt -y autoclean
